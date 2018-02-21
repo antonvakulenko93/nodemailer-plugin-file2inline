e0960742f06dc48a4cf4c7cfb30c96513d7a1ff2
anton_vakulenko <anton.vakulenko93@gmail.com>
2018-02-21 16:15:24 +0200

    Add background-url support

--- src/replace.js
+++ src/replace.js
@@ -1,7 +1,11 @@
 var fs = require("fs");
 
 function replaceFiles(options, html, getCidFunc) {
-  return html.replace(/src=(?:"|')([^"]+?)(?:"|')/g, function(fullMatch, filePath) {
+  return html.replace(/(src=(?:"|')([^"]+?)(?:"|'))|(url\((?:"|')([^'"]+?)(?:"|')\))|(background=(?:"|')([^"]+?)(?:"|'))/g, function(data, fullMatch1, filePath1, fullMatch2, filePath2, fullMatch3, filePath3) {
+
+    let fullMatch = fullMatch1 || fullMatch2 || fullMatch3;
+    let filePath = filePath1 || filePath2 || filePath3;
+
     var unwantedPrefixes = ["data:", "cid:", "http:", "https:"];
     for (var i = 0, len = unwantedPrefixes.length; i < len; i++) {
       if (filePath.indexOf(unwantedPrefixes[i]) === 0) {
@@ -16,8 +20,15 @@ function replaceFiles(options, html, getCidFunc) {
     }
 
     var cid = getCidFunc(filePath);
+    var imageAttrs = [];
+    if(fullMatch1){
+      imageAttrs.push(`src="cid:${cid}"`) 
+    } else if(fullMatch2){
+      imageAttrs.push(`url("${cid}")`) 
+    } else if (fullMatch3) {
+      imageAttrs.push(`background="cid:${cid}"`) 
+    }
 
-    var imageAttrs = ['src="cid:' + cid +'"'];
     if (options.maxWidth) {
       imageAttrs.push('width="'+options.maxWidth+'"');
     }
